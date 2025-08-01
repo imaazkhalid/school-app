aabca439626ea7c03fda7d24a1d97ac471667012
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-01 12:25:03 +0500

    update sections migration schema

--- database/migrations/2025_08_01_070016_create_sections_table.php
+++ database/migrations/2025_08_01_070016_create_sections_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Course;
+use App\Models\Teacher;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Course::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Teacher::class)->constrained()->onDelete('cascade');
+            $table->string('schedule');
+            $table->integer('capacity')->default(30);
+            $table->integer('seats_available')->default(30);
             $table->timestamps();
         });
     }
