defe80e3ee137fd3c61c54f3fa7d52b44ca42f7e
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-01 16:19:12 +0500

    add create-course component

---
+++ app/Livewire/Admin/Course/CreateModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Course;
+
+use Livewire\Component;
+
+class CreateModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.course.create-modal');
+    }
+}
