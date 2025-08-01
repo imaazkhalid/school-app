d1c897718836923afe9daa36e1df4b7d12b2b09c
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-01 13:15:53 +0500

    update Course model relationships

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'code', 'description'];
+
+    /**
+     * The sections for this course.
+     */
+    public function sections(): HasMany
+    {
+        return $this->hasMany(Section::class);
+    }
 }
