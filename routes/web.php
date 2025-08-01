03b5cdfa5132b780e507b96aaa66a39fc014458c
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-01 14:42:24 +0500

    add admin dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Admin\Dashboard as AdminDashboard;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -19,6 +20,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
