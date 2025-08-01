d29f979731e692ed225a711b804c67a30fe31879
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-01 14:38:58 +0500

    update admin dashboard controller to fetch courses

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Course;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $courses;
+
+    public function mount()
+    {
+        $this->courses = Course::withCount('sections')->get();
+    }
     public function render()
     {
         return view('livewire.admin.dashboard');
