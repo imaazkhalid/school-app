053d9b32c716ce68c242dd36ab52f980db9dcdce
Maaz Khalid <imaazkhalid@gmail.com>
2025-08-03 23:29:16 +0500

    add component for student enrollment

---
+++ app/Livewire/Student/Enroll.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Student;
+
+use Livewire\Component;
+
+class Enroll extends Component
+{
+    public function render()
+    {
+        return view('livewire.student.enroll');
+    }
+}
